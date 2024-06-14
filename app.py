b805fda66510cd0a63e7b4d870e36f7c6354ee4e
JacobRajca <jakubrca@gmail.com>
2024-06-14 23:42:18 +0200

    Pan Tadeusz Flask JR

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/k<int:ksiega_nr>')
+def ksiega(ksiega_nr):
+    return render_template(f'k{ksiega_nr}.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
